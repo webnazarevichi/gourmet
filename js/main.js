234a32569e63d467a5d34260519afdcf9040a207
webnazarevichi <86548830+webnazarevichi@users.noreply.github.com>
2021-10-10 21:12:41 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var trigger = document.getElementById('toggle');
+var box = document.getElementById('menu');
+
+toggle.addEventListener('click', function() {
+  box.classList.toggle('active');
+});
+
